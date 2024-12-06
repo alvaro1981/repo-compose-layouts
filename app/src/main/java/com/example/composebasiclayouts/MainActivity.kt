ef42a6eb910b3ac50e7692d2d76152a81c7b71a9
alvaro1981 <alvaro.alarcon.zamora@gmail.com>
2024-12-06 19:42:08 -0300

    Se finalizo el home Screen

--- app/src/main/java/com/example/composebasiclayouts/MainActivity.kt
+++ app/src/main/java/com/example/composebasiclayouts/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,7 +25,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -271,8 +274,10 @@ fun HomeSectionPreview() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier){
-    Column(modifier) {
-        //Spacer(Modifier.height(16.dp))
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())) {
+        Spacer(Modifier.height(16.dp))
         SearchBar(Modifier.padding(horizontal = 16.dp))
         HomeSection(title = R.string.align_your_body) {
             AlignYourBodyRow()
@@ -280,11 +285,11 @@ fun HomeScreen(modifier: Modifier = Modifier){
         HomeSection(title = R.string.favorite_collections) {
             FavoriteCollectionsGrid()
         }
-
+        Spacer(Modifier.height(16.dp))
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 180)
 @Composable
 fun ScreenContentPreview() {
     ComposeBasicLayoutsTheme  { HomeScreen() }
